f5669590fd57cfb518ad1b44e9de2bb4751fce06
John C. Anderson <SSTattler11@gmail.com>
2017-12-13 16:14:36 -0700

    Added timeout...
    timeout

--- kitchen.erl
+++ kitchen.erl
@@ -13,7 +13,7 @@ fridge1() ->
         {From, {take, _Food}} -> %% uh....
                                  From ! {self(), not_found},
                                  fridge1();
-                                 
+
         terminate             -> ok
     end.
 
@@ -40,4 +40,18 @@ store(Pid, Food) -> Pid ! {self(), {store, Food}},
 take(Pid, Food) ->  Pid ! {self(), {take, Food}},
                     receive
                         {Pid, Msg} -> Msg
+                    end.
+
+store2(Pid, Food) -> Pid ! {self(), {store, Food}},
+                     receive
+                         {Pid, Msg} -> Msg
+                     after 3000 ->
+                         timeout
+                     end.
+
+take2(Pid, Food) -> Pid ! {self(), {take, Food}},
+                    receive
+                        {Pid, Msg} -> Msg
+                    after 3000 ->
+                        timeout
                     end.
